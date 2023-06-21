07fd2da7878ad786ca1c599ce8514e0653bb0993
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2023-06-21 23:51:59 +0300

    1.4_course_information: change the way props are passed

--- part1/course_information/src/components/Content.js
+++ part1/course_information/src/components/Content.js
@@ -1,9 +1,7 @@
 import Part from "./Part"
 
 const Content = (props) => {
-    const part1 = props.content[0]
-    const part2 = props.content[1]
-    const part3 = props.content[2]
+    const [part1, part2, part3] = props.content
 
     return (
         <>
--- part1/course_information/src/components/Total.js
+++ part1/course_information/src/components/Total.js
@@ -1,5 +1,7 @@
 const Total = (props) => {
-    const [exercises1, exercises2, exercises3] = props.exercises
+    const exercises1 = props.parts[0].exercises
+    const exercises2 = props.parts[1].exercises
+    const exercises3 = props.parts[2].exercises
     
     return (
         <p>
